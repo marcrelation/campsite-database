a2bf1d4d816dde8d3f96deb9cd4d0e49a15b9681
Marc Relation <34357776+marcrelation@users.noreply.github.com>
2022-02-18 16:19:02 -0500

    Finding ghost sites

--- availability_finder.py
+++ availability_finder.py
@@ -39,7 +39,6 @@ def find_availability():
     SELECT park_name, park_id, area_name, area_id, group_concat(site_number)
     FROM campsites
     WHERE
-    park_name like 'Anas%' and
     vehicle_length>32 and 
     site_name like 'RV%'
     GROUP BY
@@ -57,6 +56,24 @@ def find_availability():
     ]
     con = sqlite3.connect("campsites.db")
     cur = con.cursor()
+    cur.execute('''CREATE TABLE IF NOT EXISTS availability (
+        park_id INTEGER, 
+        area_id INTEGER,
+        site_number TEXT, 
+        date TEXT)
+    ''')
+    cur.execute('''CREATE TABLE IF NOT EXISTS availability_ranges (
+        park_id INTEGER, 
+        area_id INTEGER,
+        site_number TEXT, 
+        start_date TEXT,
+        available_days INTEGER)
+    ''')
+
+    cur = con.cursor()
+    cur.execute("DELETE FROM availability")
+    cur.execute("DELETE FROM availability_ranges")
+    con.commit()
     park_name=''
     park_id=''
     facility_names=[]
@@ -71,16 +88,37 @@ def find_availability():
         sites_matching_criteria = row[4]
         for dates in year_dates:
             units = get_units(session,facility_id,dates)
-            time.sleep(2)
+            time.sleep(3)
             rows=[]
             if units is None:
                 continue
+            cur2 = con.cursor()
             for unitId,unit in units.items():
                 if unit['ShortName'] in sites_matching_criteria:
                     for slice_date,slice in unit['Slices'].items():
                         if slice['IsFree']:
                             print(f"Park: {park_name} Area: {facility_name} Site: {unit['Name']} Date: {slice['Date']} is available.")
+                            cur2.execute("INSERT INTO availability VALUES (?,?,?,?)",[park_id,facility_id,unit['ShortName'],slice['Date']])
                         #pprint.pprint(slice)
-
+            con.commit()
+    park_id=''
+    area_id=''
+    site_number=''
+    date=''
+    days=1
+    for row in cur.execute("SELECT * FROM availability ORDER BY park_id,area_id,site_number,date"):
+        if site_number=='':
+            days=1
+        elif park_id!=row[0] or area_id!=row[1] or site_number!=row[2] or (datetime.strptime(date, "%Y-%m-%d").toordinal()+1)!=datetime.strptime(row[3], "%Y-%m-%d").toordinal():
+            cur2 = con.cursor()
+            cur2.execute("INSERT INTO availability_ranges VALUES (?,?,?,?,?)",[park_id,area_id,site_number,date,days])
+            con.commit()
+            days=1
+        else:
+            days+=1
+        park_id=row[0]
+        area_id=row[1]
+        site_number=row[2]
+        date=row[3]
     con.close()
 find_availability()
--- collectors/FL/state_parks.py
+++ collectors/FL/state_parks.py
@@ -11,20 +11,21 @@ from dateutil.relativedelta import relativedelta
 # DEFAULT ARRIVAL DATE TO TODAY + 11 MONTHS
 ARRIVAL_DATE=os.getenv('ARRIVAL_DATE', (datetime.now() + relativedelta(months=11)).strftime("%m-%d-%Y"))
 
-def get_parks(session):
+# FOR SOME REASON THIS MISSES THE LAST ONE ALPHA (Wekiva)
+def get_parks_no_worky(session):
     url='https://floridardr.usedirect.com/Floridardr/rdr/search/place'
     params={
         "PlaceId":0,
         "Latitude":"28.6289",
         "Longitude":"-81.3573",
         "HighlightedPlaceId":0,
-        "StartDate":"01-14-2023",
+        "StartDate":ARRIVAL_DATE,
         "Nights":"1",
         "CountNearby":True,
         "NearbyLimit":1000,
         "NearbyOnlyAvailable":False,
-        "NearbyCountLimit":10,
-        "Sort":"Name",
+        "NearbyCountLimit":1000,
+        "Sort":"PlaceId",
         "CustomerId":"0",
         "RefreshFavourites":True,
         "IsADA":False,
@@ -37,6 +38,72 @@ def get_parks(session):
     }
     resp = session.post(url, json=params) 
     return resp.json()['NearbyPlaces']
+
+def get_parks(session):
+    return [
+        {"Name":"Alafia River State Park", "PlaceId":1},
+        {"Name":"Anastasia State Park", "PlaceId":2},
+        {"Name":"Bahia Honda State Park", "PlaceId":4},
+        {"Name":"Big Lagoon State Park", "PlaceId":6},
+        {"Name":"Blackwater River State Park", "PlaceId":8},
+        {"Name":"Blue Spring State Park", "PlaceId":9},
+        {"Name":"Caladesi Island State Park", "PlaceId":10},
+        {"Name":"Cayo Costa State Park", "PlaceId":12},
+        {"Name":"Collier-Seminole State Park", "PlaceId":13},
+        {"Name":"Colt Creek State Park", "PlaceId":14},
+        {"Name":"Curry Hammock State Park", "PlaceId":15},
+        {"Name":"Dr. Julian G. Bruce St. George Island State Park", "PlaceId":16},
+        {"Name":"Falling Waters State Park", "PlaceId":17},
+        {"Name":"Fanning Springs State Park", "PlaceId":18},
+        {"Name":"Faver-Dykes State Park", "PlaceId":19},
+        {"Name":"Florida Caverns State Park", "PlaceId":20},
+        {"Name":"Fort Clinch State Park", "PlaceId":21},
+        {"Name":"Fred Gannon Rocky Bayou State Park", "PlaceId":23},
+        {"Name":"Gamble Rogers Memorial State Recreation Area at Flagler Beach", "PlaceId":24},
+        {"Name":"Grayton Beach State Park", "PlaceId":25},
+        {"Name":"Henderson Beach State Park", "PlaceId":26},
+        {"Name":"Highlands Hammock State Park", "PlaceId":27},
+        {"Name":"Hillsborough River State Park", "PlaceId":28},
+        {"Name":"Hontoon Island State Park", "PlaceId":29},
+        {"Name":"John Pennekamp Coral Reef State Park", "PlaceId":31},
+        {"Name":"Jonathan Dickinson State Park", "PlaceId":32},
+        {"Name":"Kissimmee Prairie Preserve State Park", "PlaceId":34},
+        {"Name":"Koreshan State Park", "PlaceId":35},
+        {"Name":"Lafayette Blue Springs State Park", "PlaceId":36},
+        {"Name":"Lake Griffin State Park", "PlaceId":37},
+        {"Name":"Lake Kissimmee State Park", "PlaceId":38},
+        {"Name":"Lake Louisa State Park", "PlaceId":39},
+        {"Name":"Lake Manatee State Park", "PlaceId":40},
+        {"Name":"Little Manatee River State Park", "PlaceId":41},
+        {"Name":"Little Talbot Island State Park", "PlaceId":42},
+        {"Name":"Long Key State Park", "PlaceId":43},
+        {"Name":"Manatee Springs State Park", "PlaceId":45},
+        {"Name":"Mike Roess Gold Head Branch State Park", "PlaceId":46},
+        {"Name":"Myakka River State Park", "PlaceId":47},
+        {"Name":"Ochlockonee River State Park", "PlaceId":48},
+        {"Name":"O'Leno State Park", "PlaceId":49},
+        {"Name":"Oleta River State Park", "PlaceId":50},
+        {"Name":"Oscar Scherer State Park", "PlaceId":51},
+        {"Name":"Paynes Prairie Preserve State Park", "PlaceId":53},
+        {"Name":"Rainbow Springs State Park", "PlaceId":54},
+        {"Name":"Rodman Campground", "PlaceId":57},
+        {"Name":"Ross Prairie Trailhead and Campground", "PlaceId":58},
+        {"Name":"Ruth B. Kirby Gilchrist Blue Springs State Park", "PlaceId":59},
+        {"Name":"Santos Trailhead and Campground", "PlaceId":60},
+        {"Name":"Sebastian Inlet State Park", "PlaceId":61},
+        {"Name":"Shangri-La Trailhead and Campground", "PlaceId":62},
+        {"Name":"Silver Springs State Park", "PlaceId":63},
+        {"Name":"St. Andrews State Park", "PlaceId":64},
+        {"Name":"T.H. Stone Memorial St. Joseph Peninsula State Park", "PlaceId":65},
+        {"Name":"Stephen Foster Folk Culture Center State Park", "PlaceId":69},
+        {"Name":"Suwannee River State Park", "PlaceId":70},
+        {"Name":"Three Rivers State Park", "PlaceId":71},
+        {"Name":"Tomoka State Park", "PlaceId":72},
+        {"Name":"Topsail Hill Preserve State Park", "PlaceId":73},
+        {"Name":"Torreya State Park", "PlaceId":74},
+        {"Name":"Wekiwa Springs State Park", "PlaceId":75},
+        {"Name":"Suwannee River Wilderness Trail", "PlaceId":166}
+    ]
 
 def get_facilities(session,park):
     park_id = park['PlaceId']
@@ -151,9 +218,12 @@ def fetch_amenities(con):
                 row.append(sewer=="yes")
                 row.append(shade)
                 row.append(view)
+                row.append(unit.get("IsAda"))
+                row.append(unit.get("AllowWebBooking"))
+                row.append(unit.get("IsWebViewable"))
                 rows.append(row)
             cur = con.cursor()
-            cur.executemany("INSERT INTO campsites VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", rows)
+            cur.executemany("INSERT INTO campsites VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", rows)
             con.commit()
             # THIS IS IMPORTANT... IF YOU HIT THE SITE TO FAST YOU WILL BE BLOCKED...
 
@@ -175,7 +245,10 @@ cur.execute('''CREATE TABLE IF NOT EXISTS campsites (
     water INTEGER,
     sewer INTEGER,
     shade TEXT,
-    view text
+    view TEXT,
+    is_ada TEXT,
+    allow_web_booking TEXT,
+    is_web_viewable TEXT
     )''')
 fetch_amenities(con)
 con.close()
